28b6b20615b608cacb7288d72ad6d697ed25e772
vkenutama <work.vincentkenu@gmail.com>
2026-09-24 01:15:45 +0700

    Add a few endpoints

---
+++ ngapal-jepang-be/Endpoints/LearnEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using ngapal_jepang_be.Data;
+
+namespace ngapal_jepang_be.Endpoints
+{
+    public static class LearnEndpoint
+    {
+        public static void MapLearnEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("learn/");
+
+        }
+
+        
+    }
+}
---
+++ ngapal-jepang-be/Endpoints/UserEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using ngapal_jepang_be.Data;
+using ngapal_jepang_be.Dtos.User;
+using System.Runtime.CompilerServices;
+
+namespace ngapal_jepang_be.Endpoints
+{
+    public static class UserEndpoint
+    {
+        private static readonly string tags = "User";
+
+        public static void MapUserEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/api/user").WithTags(tags);
+
+            // Get User by Id
+            group.MapGet("/id/{userId:guid}", async (Guid? userId, AppDbContext db) =>
+            {
+                var existingUser = db.Users.Find(userId);
+
+                return existingUser == null ? Results.NotFound("User with this id not found") : Results.Ok(new GetUserDto(existingUser.Name, existingUser.Username, existingUser.PictureUrl, existingUser.CreatedAt));
+            });
+
+
+
+        }
+    }
+}
